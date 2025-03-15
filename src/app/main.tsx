9cd5cde69157c9cd8ad0f777415ccbe89aa80b5b
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-03-15 19:57:53 +0600

    feat: create card for cars data and create ui buttons

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './styles/reset.scss'
 import './styles/variables.scss'
 import './styles/global.scss'
+import { Card_item, List_item } from '@shared/components/car-item';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<>
 	<Provider />
+	<Card_item/>
+	<List_item/>
+	</>
 );
