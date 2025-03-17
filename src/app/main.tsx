ad201de638ad8d1135416182de74ac5f9bef3e02
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-03-17 15:57:07 +0600

    bugFix: fix elements on car-card

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,14 +1,14 @@
 import { Provider } from './providers';
 import ReactDOM from 'react-dom/client';
-import './styles/reset.scss'
-import './styles/variables.scss'
-import './styles/global.scss'
+import './styles/reset.scss';
+import './styles/variables.scss';
+import './styles/global.scss';
 import { Card_item, List_item } from '@shared/components/car-item';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<>
-	<Provider />
-	<Card_item/>
-	<List_item/>
-	</>
+   <>
+      <Provider />
+      <Card_item actions={[{ type: 'Подробнее' }, { type: 'Сравнение', button: { icon: { name: '' } } }]} />
+      <List_item actions={[{ type: 'Подробнее' }, { type: 'Сравнение', button: { icon: { name: '' } } }]} />
+   </>,
 );
--- src/shared/components/car-item/ui/List_item.tsx
+++ src/shared/components/car-item/ui/List_item.tsx
@@ -1,42 +1,59 @@
-import s from "../style/List_item.module.scss"
-import expamleImgForCard from "../../../../../public/image/askar-img.svg";
-import { SeeMoreBtn } from "./buttons/SeeMoreBtn";
-import { CompareBtn } from "./buttons/CompareBtn";
+import s from '../style.module.scss';
+import expamleImgForCard from '../../../../../public/image/askar-img.svg';
+import { Button, type ButtonProps } from '@shared/components/button/button.ui';
 
 const CardDatas = {
-  model: 'Kia K5',
-  range: '38 000',
-  year: '2024',
-  price: '24 410'
+   model: 'Kia K5',
+   range: '38 000',
+   year: '2024',
+   price: '24 410',
+};
+
+interface ActionButton {
+   type: 'Подробнее' | 'Сравнение';
+   button?: Omit<ButtonProps, 'children'>;
 }
 
-export const List_item = () => {
-  return (
-    <>
-    <div className={s.card}>
-      <div className={s.card_img}>
-      <img src={expamleImgForCard} alt="" />
-      </div>
-
-      <div className={s.left_cont}>
-      <div className={s.card_content}>
-        <div className={s.card_title}>
-        <h1>{CardDatas.model}</h1>
-        <div className={s.car_specs}>
-          <p>{CardDatas.range}km</p>
-          <p>.</p>
-          <p>{CardDatas.year}</p>
-        </div>
-        </div>
-        <h3>${CardDatas.price}</h3>
-      </div>
-
-      <div className={s.buttons}>
-          <CompareBtn label="Сравнить"/>
-          <SeeMoreBtn label="Подробнее"/>
-        </div>
-      </div>
-    </div>
-    </>
-  )
+interface IProps {
+   actions: ActionButton[];
 }
+
+
+export const List_item: React.FC<IProps> = ({ actions }) => {
+   return (
+      <>
+         <div className={`${s.car} ${s['t-list']}`}>
+            <div className={s.card}>
+               <div className={s.card_img}>
+                  <img src={expamleImgForCard} alt='' />
+               </div>
+
+               <div className={s.left_cont}>
+                  <div className={s.card_content}>
+                     <div className={s.card_title}>
+                        <h1>{CardDatas.model}</h1>
+                        <div className={s.car_specs}>
+                           <p>{CardDatas.range}km</p>
+                           <p>.</p>
+                           <p>{CardDatas.year}</p>
+                        </div>
+                     </div>
+                     <h3>${CardDatas.price}</h3>
+                  </div>
+
+                  <div className={s.buttons}>
+                     {actions.map(({ type, button }, index) => (
+                        <Button
+                        {...button}
+                        variant={type === 'Сравнение' ? 'outline' : 'solid'}
+                        key={type}
+                        children={type}
+                     />
+                     ))}
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+};
