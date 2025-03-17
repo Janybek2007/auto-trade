adf2824d0304710e77d042178ab1793fe9cad481
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-03-17 16:05:22 +0600

    refactor: cars-card

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -8,7 +8,7 @@ import { Card_item, List_item } from '@shared/components/car-item';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
    <>
       <Provider />
-      <Card_item actions={[{ type: 'Подробнее' }, { type: 'Сравнение', button: { icon: { name: '' } } }]} />
-      <List_item actions={[{ type: 'Подробнее' }, { type: 'Сравнение', button: { icon: { name: '' } } }]} />
+      {/* <Card_item actions={[{ type: 'Подробнее' }, { type: 'Сравнение', button: { icon: { name: '' } } }]} />
+      <List_item actions={[{ type: 'Подробнее' }, { type: 'Сравнение', button: { icon: { name: '' } } }]} /> */}
    </>,
 );
