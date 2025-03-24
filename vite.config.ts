80bb8523d4d790810fb8745e47902b358faa5b78
Janybek2007 <islam.janybekov.007@gmail.com>
2025-03-24 16:28:21 +0600

    refactor: finished base refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,42 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import compression from 'vite-plugin-compression';
 
-const isProd = process.env.NODE_ENV == 'production';
-const isDev = process.env.NODE_ENV == 'development';
-
-export default defineConfig({
-   plugins: [
-      react(),
-      TanStackRouterVite({
-         target: 'react',
-         autoCodeSplitting: true,
-         routesDirectory: './src/app/routes',
-      }),
-      tsConfigPaths({
-         projects: ['./tsconfig.json'],
-      }),
-      compression({
-         algorithm: 'brotliCompress',
-         ext: '.br',
-         threshold: 10240,
-         deleteOriginFile: false,
-      }),
-   ],
-   server: { port: 3000 },
-   build: {
-      target: 'esnext',
-      outDir: 'dist',
-      sourcemap: isDev,
-      minify: isProd ? 'terser' : false,
-      cssMinify: true,
-      ssr: false,
-   },
+export default defineConfig(({ mode }) => {
+   const env = loadEnv('develpoment', '.', '');
+   const isProd = mode === 'production';
+   const isDev = mode === 'development';
+   return {
+      plugins: [
+         react(),
+         TanStackRouterVite({
+            target: 'react',
+            autoCodeSplitting: true,
+            routesDirectory: './src/app/routes',
+         }),
+         tsConfigPaths({
+            projects: ['./tsconfig.json'],
+         }),
+         compression({
+            algorithm: 'brotliCompress',
+            ext: '.br',
+            threshold: 10240,
+            deleteOriginFile: false,
+         }),
+      ],
+      server: {
+         port: parseInt(env.PORT),
+         host: true,
+      },
+      build: {
+         target: 'esnext',
+         outDir: 'dist',
+         sourcemap: isDev,
+         minify: isProd ? 'terser' : false,
+         cssMinify: true,
+         ssr: false,
+      },
+   };
 });
